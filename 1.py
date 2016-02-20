4b5f510c1948bfc14a69ae6dd16e18aafa63f981
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-02-20 16:26:46 +0100

    Code solution to problem 1

--- 1.py
+++ 1.py
@@ -1,28 +1,42 @@
 import typing
 
-from math import sin, tan, cos
-from statistics import pstdev as pstd
-
 import numpy as np
+from numpy import sin, tan, cos, arcsin, linspace
+
 import matplotlib.pyplot as plt
 
+from math import pi
 
-def main():
-    a1 = 10
-    a2 = 10
 
-    n1 = 1
-    n2 = 1.75
+def graph(n1, n2):
+    xlimits = (-pi/2, pi/2)
+    a1 = linspace(xlimits[0], xlimits[1], 500)
+    a2 = arcsin(n1/n2 * sin(a1))
 
     R_s = abs(sin(a1 - a2)/sin(a1 + a2))**2
     R_p = abs(tan(a1 - a2)/tan(a1 + a2))**2
 
-    x = np.linspace(0, 10, 101)
-    print(x)
-    y = x**2
-    plt.plot(x, y, ".r")
+    plt.plot(a1, R_s, "r")
+    plt.plot(a1, R_p, "b")
+    plt.xlim(*xlimits)
+
+    #plt.axis().set_ticks(*xlimits)
+
+
+def main():
+    n1 = 1
+    n2 = 1.75
+
+    plt.subplot(2, 1, 1)
+    graph(n1, n2)
+
+    plt.subplot(2, 1, 2)
+    graph(n2, n1)
+
+    plt.xlabel("Radians")
     plt.show()
 
 
-main()
+if __name__ == "__main__":
+    main()
 
